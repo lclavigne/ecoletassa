c21a8890b2c5d151df322845a4b68a2edfb87c4c
Leonardo Lavigne <lclavigne@gmail.com>
2020-06-04 09:59:47 -0300

    create modal, fix bugs

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -35,6 +35,31 @@ function getCities(event) {
 
 populateUFs()
 
-document
-     .querySelector('select[name=uf]')
-     .addEventListener('change', getCities)
+document.querySelector('select[name=uf]').addEventListener('change', getCities)
+
+// Itens de Coleta
+
+const itemsToCollect = document.querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+    item.addEventListener('click', handleSelectedItem)
+}
+
+const collectedItems = document.querySelector('input[name=items]')
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle('selected')
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(item => item == itemId)
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => item != itemId)
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+    collectedItems.value = selectedItems
+}
