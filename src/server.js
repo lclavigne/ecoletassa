b31d550067a33e2c812d04f63be1d1241b530e42
Leonardo Lavigne <lclavigne@gmail.com>
2020-06-04 12:09:56 -0300

    create first routes

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const njk = require('nunjucks')
+const server = express()
+
+server.use(express.static('public'))
+
+njk.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+server.get('/', (req, res) => {
+    return res.render('index.html')
+})
+
+server.get('/create-point', (req, res) => {
+    return res.render('create-point.html')
+})
+server.listen(3000)
